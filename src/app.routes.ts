bd93b0a8eaa3604bac8c8797c2ef005f521a992d
Hugo Gonzalez <hugobedmar10@gmail.com>
2025-03-25 16:41:18 +0100

    refactor: update routing configuration to include Notfound page and redirect

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -3,22 +3,26 @@ import { AppLayout } from './app/layout/component/app.layout';
 import { AuthGuard } from './app/pages/auth/guards/auth.guard';
 import { Login } from './app/pages/auth/login';
 import { Dashboard } from './app/pages/dashboard/dashboard';
+import { Notfound } from './app/pages/notfound/notfound';
 
 export const appRoutes: Routes = [
     {
-        path: '',
+        path: 'dashboard',
         component: AppLayout,
         canActivate: [AuthGuard],
         children: [
             { path: 'dashboard', component: Dashboard, },
             { path: 'rooms', loadChildren: () => import('./app/pages/rooms/rooms.routes') }
-            /* { path: 'documentation', component: Documentation },
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') } */
         ]
     },
-    { path: 'login', component: Login }
-    /* { path: 'notfound', component: Notfound },
-    { path: '**', redirectTo: '/notfound' }, */
-    /* { path: 'landing', component: Landing },
-    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') } */
+    { path: 'login', component: Login },
+    { path: 'notfound', component: Notfound },
+    { path: '**', redirectTo: '/notfound' },
+    { path: 'auth', loadChildren: () => import('./app/pages/auth/auth.routes') } 
 ];
+
+
+
+
+/* { path: 'documentation', component: Documentation },
+            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') } */
