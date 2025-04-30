3f4c3617f2e291ffa1e5ecc62c7db1ee10283cb9
Joan Rodrigues <joanlima2005@gmail.com>
2025-04-30 16:44:32 +0200

    feat: Add button to navigate to cares page with patient ID in insideRooms component

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -2,10 +2,10 @@ import { Routes } from '@angular/router';
 import { AppLayout } from './app/layout/component/app.layout';
 import { AuthGuard } from './app/pages/auth/guards/auth.guard';
 import { Login } from './app/pages/auth/login';
+import { Cares } from './app/pages/cares/cares';
 import { Dashboard } from './app/pages/dashboard/dashboard';
 import { Diets } from './app/pages/diets/diets';
 import { Notfound } from './app/pages/notfound/notfound';
-import { Cares } from './app/pages/cares/cares';
 
 export const appRoutes: Routes = [
     {
@@ -13,11 +13,11 @@ export const appRoutes: Routes = [
         component: AppLayout,
         canActivate: [AuthGuard],
         children: [
-            { path: 'dashboard', component: Dashboard, },
+            { path: 'dashboard', component: Dashboard },
             { path: 'rooms', loadChildren: () => import('./app/pages/rooms/rooms.routes') },
             { path: 'diets', component: Diets },
-            { path: 'cares', component: Cares, },
-            { path: 'extras', loadChildren: () => import('./app/pages/pages.routes') },
+            { path: 'cares/:id', component: Cares },
+            { path: 'extras', loadChildren: () => import('./app/pages/pages.routes') }
         ]
     },
     { path: 'login', component: Login },
--- src/app/pages/rooms/insideRoom.ts
+++ src/app/pages/rooms/insideRoom.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { CardModule } from 'primeng/card';
 import { PaginatorModule } from 'primeng/paginator';
@@ -16,24 +16,35 @@ import { RoomsService } from '../../service/rooms.service';
             <div class="grid lg:grid-cols-2 sm:grid-cols-1 gap-10 justify-items-center pb-10">
                 <h1>Numero de room: {{ room_id }}</h1>
             </div>
+            <div>
+                <!-- Implementar boton que te redirija a cares, mandando el id de el usuario a hacer la care. Tomar como ejemplo el realizado para acceder a dentro de la rooms, pasando el id de room -->
+                <button class="p-3 border rounded-xl bg-blue-700 text-white w-1/4" (click)="openCares(room[0].paciente.pac_id)">Cares</button>
+            </div>
         </div>
     `
 })
 export class insideRooms implements OnInit {
+
     room_id: string | null = null;
+    room: any[] = [];
 
     constructor(
         private rs: RoomsService,
-        private route: ActivatedRoute
+        private route: ActivatedRoute,
+        private router: Router
     ) {}
 
     ngOnInit(): void {
         this.room_id = this.route.snapshot.paramMap.get('id');
-        console.log(this.room_id);
         if (this.room_id) {
             this.rs.getRoom(this.room_id).subscribe((data: any) => {
-                console.log(data);
+                this.room = data;
             });
         }
     }
+
+    openCares(pac_id: string) {
+        this.router.navigate(['/cares/', pac_id]);
+    }
+
 }
